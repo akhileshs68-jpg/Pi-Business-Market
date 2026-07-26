386af71e7e15a6a59c9d8ff83732d9a8964835fb
Akhilesh Singh <akhileshs68@gmail.com>
2026-07-26 23:11:54 +0530

    Debug Firebase environment variables

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -11,6 +11,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
